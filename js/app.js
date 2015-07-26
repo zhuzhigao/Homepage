8b720728e89fa9277e31e639cd415957c127b9b4
Zhigao Zhu <zhigao.zhu@gmail.com>
2015-07-26 18:13:32 +0800

    add footprint location

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ app.config(['$routeProvider',
         controller: 'homeCtrl'
       }).
       when('/footprint', {
-        templateUrl: './pages/footprint.html',
-        controller: 'homeCtrl'
+        // templateUrl: './pages/footprint.html',
+        template: '<div ng-include src="templateUrl" onload="onsliderload()"></div>',
+        controller: 'footprintCtrl'
       }).
       otherwise({
       	//templateUrl doesn't work in this case because there is on onload event. but ng-include has and will do binding with the controller.
@@ -50,4 +51,36 @@ app.controller('homeCtrl', function($scope) {
                 })
             }), 50)
         }
+});
+
+app.controller('footprintCtrl', function($scope) {
+	$scope.templateUrl = "./pages/footprint.html";
+	
+	//this must be called to enable the slider. 
+	//the time delay is necessary to make sure the element is in place. 
+	$scope.onsliderload = function() {
+		setTimeout(function(){
+			var map = new BMap.Map("baidumap");  
+			map.centerAndZoom(new BMap.Point(0, 20), 1);
+			map.addControl(new BMap.MapTypeControl());
+			map.setCurrentCity("北京");          
+			map.enableScrollWheelZoom(true);
+			var points = [
+				{x: 99.67, y: 6.35},//lankawei
+				{x: 115.22, y: -8.8},//bali
+				{x: -95.4, y: 29.78},//houston
+				{x: 101.7, y: 3.13},//kl
+				{x: 103.82, y: 1.35},//sigapore
+				{x: 104.05, y: 30.58},//chendu
+				{x: 110.48, y: 29.12},//zhangjiajie
+				{x: 126.55, y: 45.6},//haerbin
+				{x: 3.88, y: 43.6}, //monpelier
+			];
+			for (var i = points.length - 1; i >= 0; i--) {
+				var marker = new BMap.Marker(new BMap.Point(points[i].x, points[i].y));  
+				map.addOverlay(marker);              
+			};
+
+		}, 50);
+	}
 });
